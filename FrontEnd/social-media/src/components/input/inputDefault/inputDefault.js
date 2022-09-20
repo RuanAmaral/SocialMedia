7df404e3f4d8b7a906f7c29bb387ea9c15e5df0e
Ruan <ruan.amaral.lemos@gmail.com>
2022-09-20 14:56:59 -0300

    subindo component InputDefault sem o style

---
+++ FrontEnd/social-media/src/components/input/inputDefault/inputDefault.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export const inputDefault = ({
+  typeI,
+  autocomplete,
+  disable,
+  max,
+  min,
+  maxlength,
+  minlength, 
+  value,
+  placeholder
+}) => {
+  return (
+    <input
+      type={typeI}
+      autoComplete={autocomplete ? autocomplete : "on"}
+      {...(disable ? "disabled" : null)}
+      value ={value}
+      placeholder = {placeholder ? (placeholder) : ("")}
+      {... max? (`max="${max}"`):("")}
+      {... min? (`min="${min}"`):("")}
+      {... maxlength? (`maxlength="${maxlength}"`):("")}
+      {... minlength? (`minlength="${minlength}"`):("")}
+    ></input>
+  );
+};
